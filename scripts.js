1c1fd88de093445a0f8d4be1b5e08d283acb594c
CLEVERTON ALLAN DE MELO <CLEVERTON_ALLAN@OUTLOOK.COM>
2024-07-16 00:13:09 -0300

    arquivos do projeto

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "60%"
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-60%"
+}
